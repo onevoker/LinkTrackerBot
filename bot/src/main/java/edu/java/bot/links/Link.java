9455d965485b08a974eecd9d178598066693882d
Alexandr Lankut <lankut.alex@gmail.com>
2024-02-22 02:25:47 +0300

    Fixed misspell in gitHub name

--- bot/src/main/java/edu/java/bot/links/Link.java
+++ bot/src/main/java/edu/java/bot/links/Link.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public record Link(Long userId, String stringLink) {
-    private static final String GITHUB_DOMAIN = "github.com";
+    private static final String GIT_HUB_DOMAIN = "github.com";
     private static final String STACK_OVERFLOW_DOMAIN = "stackoverflow.com";
 
     public Link(Long userId, String stringLink) {
@@ -36,7 +36,8 @@ public record Link(Long userId, String stringLink) {
     private boolean isValidResource(String link) {
         try {
             URI uri = new URI(link);
-            return Objects.equals(uri.getHost(), GITHUB_DOMAIN) || Objects.equals(uri.getHost(), STACK_OVERFLOW_DOMAIN);
+            return Objects.equals(uri.getHost(), GIT_HUB_DOMAIN) ||
+                Objects.equals(uri.getHost(), STACK_OVERFLOW_DOMAIN);
         } catch (URISyntaxException e) {
             return false;
         }
