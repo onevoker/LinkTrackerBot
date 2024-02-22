137b93efd2a8e71fa004f83959a58db5153af875
Alexandr Lankut <lankut.alex@gmail.com>
2024-02-22 17:00:24 +0300

    Fixed wireMocks in poms

--- scrapper/src/main/java/edu/java/gitHub/client/GitHubClient.java
+++ scrapper/src/main/java/edu/java/gitHub/client/GitHubClient.java
@@ -1,8 +1,21 @@
 package edu.java.gitHub.client;
 
 import edu.java.gitHub.dto.RepositoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-public interface GitHubClient {
-    Mono<RepositoryResponse> fetchRepository(String owner, String repository);
+@Component
+@RequiredArgsConstructor
+public class GitHubClient {
+    private final WebClient gitHubWebClient;
+
+    public Mono<RepositoryResponse> fetchRepository(String owner, String repository) {
+        return gitHubWebClient.get()
+            .uri(owner + "/" + repository)
+            .retrieve()
+            .bodyToMono(RepositoryResponse.class);
+    }
 }
--- scrapper/src/main/java/edu/java/stackOverflow/client/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/stackOverflow/client/StackOverflowClient.java
@@ -1,8 +1,20 @@
 package edu.java.stackOverflow.client;
 
 import edu.java.stackOverflow.dto.QuestionResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-public interface StackOverflowClient {
-    Mono<QuestionResponse> fetchQuestion(long questionId);
+@Component
+@AllArgsConstructor
+public class StackOverflowClient {
+    private final WebClient stackOverflowWebClient;
+
+    public Mono<QuestionResponse> fetchQuestion(long questionId) {
+        return stackOverflowWebClient.get()
+            .uri(questionId + "?order=desc&sort=activity&site=stackoverflow")
+            .retrieve()
+            .bodyToMono(QuestionResponse.class);
+    }
 }
