407ad13f2a785921a01269353593af4d3b9d0d2b
Alexandr Lankut <lankut.alex@gmail.com>
2024-02-26 22:51:36 +0300

    Fixed scheduling annotation

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -2,11 +2,13 @@ package edu.java.scheduler;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 @Log4j2
+@Component
 public class LinkUpdaterScheduler {
-    @Scheduled(fixedDelayString = "#{scheduler.forceCheckDelay()}")
+    @Scheduled(fixedDelayString = "#{scheduler.interval()}")
     public void update() {
-        log.info("checked updates in the database");
+        log.info("Checked updates in the database");
     }
 }
