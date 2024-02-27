649b1eb906b83134dd8033bbe3ae9b6c9cbd6045
Alexandr Lankut <lankut.alex@gmail.com>
2024-02-27 23:16:13 +0300

    Added test for processing null

--- bot/src/test/java/bot/processorTest/UserMessageProcessorImplTest.java
+++ bot/src/test/java/bot/processorTest/UserMessageProcessorImplTest.java
@@ -12,6 +12,7 @@ import edu.java.bot.commands.ListCommand;
 import edu.java.bot.commands.StartCommand;
 import edu.java.bot.commands.TrackCommand;
 import edu.java.bot.commands.UntrackCommand;
+import edu.java.bot.exceptions.BlockedChatException;
 import edu.java.bot.links.LinkFactory;
 import edu.java.bot.links.LinkValidatorService;
 import edu.java.bot.processor.UserMessageProcessorImpl;
@@ -25,6 +26,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,8 +51,6 @@ public class UserMessageProcessorImplTest {
     private void setUpMocksWithMessageFromTelegram(String messageFromTelegram) {
         doReturn(message).when(update).message();
         doReturn(messageFromTelegram).when(message).text();
-        doReturn(chat).when(message).chat();
-        doReturn(-1L).when(chat).id();
     }
 
     @BeforeEach
@@ -73,6 +73,8 @@ public class UserMessageProcessorImplTest {
     @Test
     void testProcessUnknownCommand() {
         setUpMocksWithMessageFromTelegram("привет");
+        doReturn(chat).when(message).chat();
+        doReturn(-1L).when(chat).id();
 
         String unknownCommandText =
             "Мне не известна эта команда, для получения доступных команд воспользуйтесь командой /help";
@@ -85,6 +87,8 @@ public class UserMessageProcessorImplTest {
     @Test
     void testProcessTrackCommand() {
         setUpMocksWithMessageFromTelegram("/track " + GIT_HUB);
+        doReturn(chat).when(message).chat();
+        doReturn(-1L).when(chat).id();
         doReturn(USER).when(message).from();
 
         String expectedHandleText = "Начали отслеживать данную ссылку";
@@ -97,6 +101,8 @@ public class UserMessageProcessorImplTest {
     @Test
     void testProcessUntrackCommand() {
         setUpMocksWithMessageFromTelegram("/untrack " + GIT_HUB);
+        doReturn(chat).when(message).chat();
+        doReturn(-1L).when(chat).id();
         doReturn(USER).when(message).from();
 
         String expectedHandleText = "Вы не отслеживаете данную ссылку";
@@ -109,6 +115,8 @@ public class UserMessageProcessorImplTest {
     @Test
     void testProcessStartCommand() {
         setUpMocksWithMessageFromTelegram("/start");
+        doReturn(chat).when(message).chat();
+        doReturn(-1L).when(chat).id();
         doReturn(USER).when(message).from();
 
         String expectedHandleText = "Начинаем регистрацию...\nДля получения списка команд используйте /help";
@@ -121,6 +129,8 @@ public class UserMessageProcessorImplTest {
     @Test
     void testProcessListCommand() {
         setUpMocksWithMessageFromTelegram("/list");
+        doReturn(chat).when(message).chat();
+        doReturn(-1L).when(chat).id();
         doReturn(USER).when(message).from();
 
         String expectedHandleText = "Вы не отслеживаете ни одной ссылки(((";
@@ -133,6 +143,8 @@ public class UserMessageProcessorImplTest {
     @Test
     void testProcessHelpCommand() {
         setUpMocksWithMessageFromTelegram("/help");
+        doReturn(chat).when(message).chat();
+        doReturn(-1L).when(chat).id();
 
         String expectedHandleText =
             "На данный момент поддерживается отслеживание ссылок с таких ресурсов как github и stackoverflow\n\n"
@@ -146,5 +158,12 @@ public class UserMessageProcessorImplTest {
         SendMessage expected = new SendMessage(-1L, expectedHandleText).parseMode(ParseMode.HTML);
 
         assertThat(result.toWebhookResponse()).isEqualTo(expected.toWebhookResponse());
+    }
+
+    @Test
+    void testProcessNull() {
+        setUpMocksWithMessageFromTelegram(null);
+        var exception = assertThrows(BlockedChatException.class, () -> messageProcessor.process(update));
+        assertThat(exception.getMessage()).isEqualTo("Chat is null, because bot was blocked");
     }
 }
